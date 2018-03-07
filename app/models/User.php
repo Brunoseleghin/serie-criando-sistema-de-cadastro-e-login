72f7e41e6137c3d9d642e7a5e5ef34775393774c
Bruno Seleghin <brunoseleghin@gmail.com>
2018-03-07 14:41:00 -0300

    Estrutura inicial de login com o mecanismo de tentativas

--- app/models/User.php
+++ app/models/User.php
@@ -2,73 +2,103 @@
 
 class User extends \HXPHP\System\Model
 {
-	static $validates_presence_of = array(
-		array(
-			'name',
-			'message' => 'O nome é um campo obrigatório.'
-		),
-		array(
-			'email',
-			'message' => 'O e-mail é um campo obrigatório.'
-		),
-		array(
-			'username',
-			'message' => 'O nome de usuário é um campo obrigatório.'
-		),
-		array(
-			'password',
-			'message' => 'A senha é um campo obrigatório.'
-		)
-	);
+    static $validates_presence_of = array
+    (
+        array
+        (
+            'name',
+            'message' => 'O nome é um campo obrigatório.'
+        ),
+        array
+        (
+            'email',
+            'message' => 'O e-mail é um campo obrigatório.'
+        ),
+        array
+        (
+            'username',
+            'message' => 'O nome de usuário é um campo obrigatório.'
+        ),
+        array
+        (
+            'password',
+            'message' => 'A senha é um campo obrigatório.'
+        )
+    );
 
-	static $validates_uniqueness_of = array(
-            array(
-                'username',
-                'message' => 'Já existe um usuário com este nome de usuário cadastrado.'
-            ),
-            array(
-                'email',
-                'message' => 'Já existe um usuário com este e-mail cadastrado.'
-            )
-	);
+    static $validates_uniqueness_of = array
+    (
+        array
+        (
+            'username',
+            'message' => 'Já existe um usuário com este nome de usuário cadastrado.'
+        ),
+        array
+        (
+            'email',
+            'message' => 'Já existe um usuário com este e-mail cadastrado.'
+        )
+    );
 
-	public static function cadastrar(array $post)
-	{
-		$callbackObj = new \stdClass;
-		$callbackObj->user = null;
-		$callbackObj->status = false;
-		$callbackObj->errors = array();
+    public static function cadastrar(array $post)
+    {
+        $callbackObj = new \stdClass;
+        $callbackObj->user = null;
+        $callbackObj->status = false;
+        $callbackObj->errors = array();
+    
+        $role = Role::find_by_role('user');
+	
+        if (is_null($role)) 
+        {
+            array_push($callbackObj->errors, 'A role user não existe. Contate o administrador');
+            return $callbackObj;
+        }
 
-		$role = Role::find_by_role('user');
+        $user_data = array
+        (
+            'role_id' => $role->id,
+            'status' => 1
+        );
 
-		if (is_null($role)) {
-			array_push($callbackObj->errors, 'A role user não existe. Contate o administrador');
-			return $callbackObj;
-		}
+        $password = \HXPHP\System\Tools::hashHX($post['password']);
 
-		$user_data = array(
-			'role_id' => $role->id,
-			'status' => 1
-		);
+        $post = array_merge($post, $user_data, $password);
 
-		$password = \HXPHP\System\Tools::hashHX($post['password']);
+        $cadastrar = self::create($post);
 
-		$post = array_merge($post, $user_data, $password);
-
-		$cadastrar = self::create($post);
-
-		if ($cadastrar->is_valid()) {
-			$callbackObj->user = $cadastrar;
-			$callbackObj->status = true;
-			return $callbackObj;
-		}
-
-		$errors = $cadastrar->errors->get_raw_errors();
-
-		foreach ($errors as $field => $message) {
-			array_push($callbackObj->errors, $message[0]);
-		}
-
-		return $callbackObj;
-	}
+        if ($cadastrar->is_valid()) 
+        {
+            $callbackObj->user = $cadastrar;
+            $callbackObj->status = true;
+            return $callbackObj;
+        }
+                
+        $errors = $cadastrar->errors->get_raw_errors();
+                
+        foreach ($errors as $field => $message) 
+        {
+            array_push($callbackObj->errors, $message[0]);
+        }
+          
+        return $callbackObj;
+    }
+    
+    public static function login(array $post)
+    {
+        $user = self::find_by_username($post['username']);
+        
+        if(!is_null($user)) 
+        {
+            $password = \HXPHP\System\Tools::hashHX($post['password'], $user->salt);
+            
+            if(LoginAttempt::ExistemTentativas($user->id)) {
+              if ($password['password'] === $user->password) {
+                LoginAttempt::LimparTentativas($user->id);
+              } 
+            } else {
+                LoginAttempt::RegistrarTentativa($user->id);
+            }
+        }
+    }
 }
